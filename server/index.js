6d5bbe806e4c1350ee769277bfde7ec3cd494b33
Hsing-Tzu <daisyko1234@gmail.com>
2023-12-17 19:23:20 +0800

    update

--- server/index.js
+++ server/index.js
@@ -136,12 +136,6 @@ app.get('/api/vesselTraceData', async (req, res) => {
   }
 });
 
-app.use(express.static("../client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname,"..", "client", "build", "index.html"))
-});
-
-
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
 });
